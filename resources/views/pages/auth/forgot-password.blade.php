f8079cde0f52de80b3b6979d018e07d85bdf489d
DiogoTorresDevelopment <100326232+DiogoTorresDevelopment@users.noreply.github.com>
2025-06-08 20:17:40 -0300

    Add JWT registration and password reset APIs

--- resources/views/pages/auth/forgot-password.blade.php
+++ resources/views/pages/auth/forgot-password.blade.php
@@ -2,13 +2,17 @@
 
 @section('content')
 <div class="page-content d-flex align-items-center justify-content-center">
-
   <div class="row w-100 mx-0 auth-page">
     <div class="col-md-4 col-xl-4 mx-auto">
       <div class="card">
         <div class="row">
           <div class="col-md-12">
             <div class="auth-form-wrapper px-3 py-4">
+              @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+              @endif
               @if ($errors->any())
                 <div class="alert alert-danger">
                   <ul>
@@ -18,21 +22,14 @@
                   </ul>
                 </div>
               @endif
-              <form method="POST" action="{{ route('login') }}">
+              <form method="POST" action="{{ route('password.email') }}">
                 @csrf
                 <div class="mb-3">
                   <label for="email" class="form-label">E-mail</label>
-                  <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required autofocus placeholder="concessionaria@gmail.com">
-                </div>
-                <div class="mb-3">
-                  <label for="password" class="form-label">Senha</label>
-                  <input type="password" class="form-control" id="password" name="password" required autocomplete="current-password" placeholder="••••••••••">
-                </div>
-                <div class="text-center mb-3">
-                  <a href="{{ route('password.request') }}" class="text-muted">Esqueceu sua senha?</a>
+                  <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required autofocus>
                 </div>
                 <div>
-                  <button type="submit" class="btn btn-primary w-100">Entrar</button>
+                  <button type="submit" class="btn btn-primary w-100">Enviar link de recuperação</button>
                 </div>
               </form>
             </div>
@@ -41,6 +38,6 @@
       </div>
     </div>
   </div>
-
 </div>
 @endsection
+
